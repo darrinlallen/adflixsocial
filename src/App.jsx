6ddcc21e2997e3e0f6ec5e61faf56999aaa0497c
darrinlallen <darrin.lovard.allen@gmail.com>
2025-07-11 09:52:07 -0700

    svg image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 
-// src/App.jsx new
-//ad flix app
-import { useEffect } from 'react'
-import gsap from 'gsap'
-import './App.css'
+import React, { useEffect } from 'react';
+import gsap from 'gsap';
+import './App.css';
+import image from './IMG_0991.svg'; // Ensure the path is correct
 
 function App() {
   useEffect(() => {
@@ -13,78 +12,57 @@ function App() {
     const subtitleContainer = document.querySelector('.subtitle-container');
 
     const html = [...title].map(letter => {
-      return letter === " " 
-        ? `<span class="letter space">&nbsp;</span>`
-        : `<span class="letter">${letter}</span>`;
+      return `<span class="letter">${letter}</span>`;
     }).join('');
 
     const subtitleHtml = [...subtitle].map(letter => {
-      return letter === " " 
-        ? `<span class="subtitle-letter space">&nbsp;</span>`
-        : `<span class="subtitle-letter">${letter}</span>`;
+      return `<span class="subtitle-letter">${letter}</span>`;
     }).join('');
 
     container.innerHTML = html;
     subtitleContainer.innerHTML = subtitleHtml;
 
-    // Main title animation
     const tl = gsap.timeline({
-      repeat: 0,
-      ease: "elastic.out(1, 0.3)"
-    });
-
-    tl.from(".letter", {
-      y: -200,
-      opacity: 0,
-      duration: 1.5,
-      stagger: {
-        amount: 0.8,
-        ease: "bounce.out"
-      },
-      rotation: 360,
-      scale: 0
-    })
-    .to(".letter", {
-      y: 0,
-      scale: 1.2,
-      duration: 0.4,
-      stagger: {
-        amount: 0.2
-      }
-    });
-
-    // Subtitle animation
-    gsap.from(".subtitle-letter", {
-      opacity: 0,
-      y: 20,
-      duration: 1,
-      stagger: {
-        amount: 0.5,
-        ease: "power2.out"
-      },
-      delay: 1.5 // Starts after main title animation
-    });
-
-    // Continuous bounce animation for main title
-    gsap.to(".letter", {
-      y: -30,
-      duration: 0.5,
-      stagger: {
-        amount: 0.5,
-        repeat: -1,
-        yoyo: true,
-        ease: "power1.inOut"
-      },
-      repeat: -1,
-      yoyo: true,
+      repeat: -1, // Repeat indefinitely
+      yoyo: true, // Yoyo effect for back-and-forth
       ease: "power1.inOut"
     });
 
-  }, [])
+    tl.fromTo(".letter", {
+      opacity: 0,
+      y: -50
+    }, {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.1
+    });
+
+    gsap.fromTo(".subtitle-letter", {
+      opacity: 0,
+      y: 20
+    }, {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.05,
+      repeat: -1,
+      yoyo: true,
+      ease: "power1.inOut",
+      delay: 1.5
+    });
+
+    gsap.to(".svg-container", {
+      opacity: 1,
+      duration: 1,
+      delay: 5
+    });
+    
+  }, []);
 
   return (
     <div className="app-container">
-      <div className="content-wrapper">
+      <div className="text-container">
         <div className="title-container">
           {/* Main title animation will handle this */}
         </div>
@@ -92,8 +70,11 @@ function App() {
           {/* Subtitle animation will handle this */}
         </div>
       </div>
+      <div className="svg-container">
+        <img src={image} alt="Description of SVG" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
