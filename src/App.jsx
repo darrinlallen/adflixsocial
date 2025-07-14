082e3e40c93e4bb50a83cdb0ff05fe0b9e414586
Darrin Allen <darrin.lovard.allen@gmail.com>
2025-07-13 22:53:39 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
         
         <div ref={videoRef} className="video-container">
           <video 
-            src="/glass.mp4" 
+            src="/Ninja.mp4" 
             autoPlay 
             loop 
             muted 
